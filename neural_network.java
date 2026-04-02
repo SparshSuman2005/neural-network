d507d00d51c349c60ca1996cd24c7f8beef23692
SparshSuman2005 <sparsh.storage1@gmail.com>
2026-04-03 01:20:14 +0530

    refactor loss calculation and update weight adjustment logic

--- neural_network.java
+++ neural_network.java
@@ -7,17 +7,26 @@ public class neural_network{
     static double[] x = {4.0,5.0};//inputs
     double[] weighths={3.0,3.0};//weights 
     double bias = 1.0;
-    double error ;
+    double loss;
 
     //activation function
     public double  sigmoid (double z){
         return (1/(1+Math.exp(-z)));
     }
 
+    public static double binary_cross_entropy(double y , double y_pred){
+
+        double loss = -(y*Math.log(y_pred) + (1-y)*Math.log(1-y_pred)); 
+        return loss;
+    }
+
 
     //main function
     public static void main(String[] args){
+
+        //instantiation
         neural_network nn = new neural_network();
+
         boolean flag = true;
         int count = 0;
         while(flag){
@@ -25,23 +34,29 @@ public class neural_network{
         int expected_output = 1;//target value
         double pred = nn.sigmoid(z);
         System.out.println("The prediction is "+pred);//predicted value
-        nn.error =pred - expected_output;
-        System.out.println("The loss is "+nn.error);
+        nn.loss = binary_cross_entropy(expected_output, pred);
+        int i;
+        double gradient = 0;
+        for(i = 0 ; i < nn.weighths.length ; i++){
 
-        double sensitivity = pred*(1-pred);
+            gradient = 0;
+            gradient =  (nn.x[i]*(pred-expected_output));
+            
+            nn.weighths[i] = nn.weighths[i] - (lr*gradient); 
 
-
-        for(int i = 0 ; i <= nn.weighths.length-1 ; i++){
-            nn.weighths[i] = nn.weighths[i]-(nn.error*nn.x[i]*lr*sensitivity);
         }
-        nn.bias = nn.bias - (nn.error*lr*sensitivity);
 
-        if(Math.abs(nn.error)<Math.pow(10,-5)){
+        if(Math.abs(nn.loss)<Math.pow(10,-5)){
             flag = false;
         }
         count++;
         if(count >= 200){
             break;
+        }
+
+        System.out.println("tye weights are ");
+        for(int j = 0 ; j < nn.weighths.length ; j++){
+            System.out.println(nn.weighths[j]);
         }
     }
     }
